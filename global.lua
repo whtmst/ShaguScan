827f69e8df31a1c96ca718062af97f1881cf29fe
Wht Mst <ven000mus@gmail.com>
2025-10-19 14:56:25 +0400

    Add 'locked' property to NPC configurations
    Added 'locked' property to Infight NPCs and Raid Targets configurations.

--- global.lua
+++ global.lua
@@ -3,12 +3,16 @@ ShaguScan_db = {
   config = {
     ["Infight NPCs"] = {
       filter = "npc,infight",
-      scale = 1, anchor = "CENTER", x = -240, y = 120, width = 100, height = 14, spacing = 4, maxrow = 20
+      scale = 1, anchor = "CENTER", x = -240, y = 120, 
+      width = 100, height = 14, spacing = 4, maxrow = 20,
+      locked = false
     },
 
     ["Raid Targets"] = {
       filter = "icon,alive",
-      scale = 1, anchor = "CENTER", x = 240, y = 120, width = 100, height = 14, spacing = 4, maxrow = 20
+      scale = 1, anchor = "CENTER", x = 240, y = 120, 
+      width = 100, height = 14, spacing = 4, maxrow = 20,
+      locked = false
     }
   }
 }
